e801ca0a12f96a8aca11d288639e3b0981d3320f
Ria Kusmara <89031250+riakusmaraa@users.noreply.github.com>
2022-04-14 10:04:46 +0700

    Update and rename Tugas3c.java to MenghitungPersamaan.java

---
+++ MenghitungPersamaan.java
@@ -0,0 +1,19 @@
+public class MenghitungPersamaan {
+    public static void main(String[] args) {
+        //deklarasi
+        int x, y, z;
+        int a = 4;
+        int b = 6;
+
+
+        //proses
+        x = 4*b;
+        y = 2*a;
+        z = (a*x)+(b*y*y);
+
+        //output
+        System.out.println("Nilai x: "+x);
+        System.out.println("Nilai y: "+y);
+        System.out.println("Nilai z: "+z);
+    }
+}
