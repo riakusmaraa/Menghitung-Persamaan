0fd0d56afd6ced73dec878a52cf19ec9f29f3447
Ria Kusmara <89031250+riakusmaraa@users.noreply.github.com>
2022-04-14 10:02:52 +0700

    Add files via upload

---
+++ Tugas3c.java
@@ -0,0 +1,19 @@
+public class Tugas3c {
+    public static void main(String[] args) {
+    //input
+    int x, y, z;
+    int a = 4;
+    int b = 6;
+ 
+    
+    //proses
+    x = 4*b;
+    y = 2*a;
+    z = (a*x)+(b*y*y);
+  
+    //output
+    System.out.println("Nilai x: "+x);
+    System.out.println("Nilai y: "+y);
+    System.out.println("Nilai z: "+z);
+}
+}
